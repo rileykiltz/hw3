6d8c378b2ceea4764270029b00723c14bc12b534
Riley Kiltz <Riley.kiltz@kellogg.northwestern.edu>
2024-05-16 16:53:47 +0000

    Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Places routes
+  get "/places", to: "places#index"
+  get "/places/new", to: "places#new", as: "new_place"
+  get "/places/:id", to: "places#show", as: "place"
+  post "/places", to: "places#create"
 
-  # Defines the root path route ("/")
-  # get("/", { :controller => "articles", :action => "index" })
+  # Entries routes
+  get "/places/:place_id/entries/new", to: "entries#new", as: "new_place_entry"
+  post "/places/:place_id/entries", to: "entries#create", as: "place_entries"
+
+  # Set the root path
+  root "places#index"
 end
